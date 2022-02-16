1018c1706d5bb3b1a5b18afb9d359177d472e77a
Eric Santos <eric.silvasantos44@gmail.com>
2022-02-16 18:57:37 -0300

    primeira e segunda parte do exercicio

---
+++ Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+const compromissos = ['Comer', 'Estudar', 'Viajar', 'Correr']
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <ol>
+        {compromissos.map((compromisso) => Task(compromisso))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
