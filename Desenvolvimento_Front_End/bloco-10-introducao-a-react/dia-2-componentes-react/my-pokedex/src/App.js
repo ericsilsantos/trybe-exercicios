274ce64377aaa5a3bcfec06bc5638af38ce51818
Eric Santos <eric.silvasantos44@gmail.com>
2022-02-18 13:47:07 -0300

    Finalizando o exercicio 10.2

--- Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,14 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-// import data from './data'
+import data from './data'
 import Pokedex from './Pokedex'
 
 function App() {
   return (
-    <div>
+    <main>
       <h1>Pokedex</h1>
-      <Pokedex />
-    </div>
+      <div>
+        {data.map((pokemon) => 
+          <Pokedex 
+            key={pokemon.id} 
+            nome={pokemon.name} 
+            tipo={pokemon.type}
+            peso={pokemon.averageWeight.value}
+            unidadePeso={pokemon.averageWeight.measurementUnit}
+            imagem={pokemon.image}
+          />
+        )}
+      </div>
+    </main>
   );
 }
 
--- Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokedex.jsx
+++ Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokedex.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import data from './data'
+import PropTypes from 'prop-types';
 
 class Pokedex extends React.Component{
     render(){
         return(
-            data.map((pokemon) => 
-            <section className="pokemon">
-                <div>
-                    <p>Nome: {pokemon.name} </p>
-                    <p>Tipo:{pokemon.type} </p>
-                    <p>Peso: {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
+            <section className="pokemon" >
+                <div className="flex-column">
+                    <p>Nome: {this.props.nome} </p>
+                    <p>Tipo: {this.props.tipo} </p>
+                    <p>Peso: {this.props.peso} {this.props.unidadePeso}</p>
                 </div>
                 <div>
-                    <img src={pokemon.image} alt="" />
+                    <img src={this.props.imagem} alt="" />
                 </div>
             </section>
-            )
         );
     }
+}
+
+Pokedex.propTypes = {
+    nome: PropTypes.string,
+    tipo: PropTypes.string,
+    peso: PropTypes.number,
+    unidadePeso: PropTypes.string,
+    imagem: PropTypes.string
 }
 
 export default Pokedex;
